655dd0e3086940ab8e3f907c4cd33ecfb330f32f
kari89diaz <kari89diaz@gmail.com>
2019-03-13 12:51:06 -0300

    Adding values of an array

---
+++ AddArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = new string[5];
+            for (int x = 0; x < nombres.Length; x++)
+            {
+                nombres[x] = (Console.ReadLine());
+            }
+            for (int x = 0; x < nombres.Length; x++)
+            {
+                Console.WriteLine(nombres[x]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
